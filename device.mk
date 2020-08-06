0bde5ec5b06031aef3fd82993b58a49b13ef400c
faham1997 <nafidfaham08@gmail.com>
2020-08-06 21:58:19 +0600

    lavender : remove XiaomiParts * remove all the doze settings since most of them don't work or useless * Rebranded XiaomiParts to MiSoundEnhancer Keeping only the Dirac Configs * Now we are only building MiSoundEnhancer
    Signed-off-by: faham1997 <nafidfaham08@gmail.com>

--- device.mk
+++ device.mk
@@ -170,9 +170,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.configstore@1.1-service
     
-# Device-specific settings
+# Dirac
 PRODUCT_PACKAGES += \
-    XiaomiParts
+    MiSoundEnhancer
 
 # Display
 PRODUCT_PACKAGES += \
